6d76b533b0c0702c3d3c705234b50e369b2d71a3
cleitonbalonekr <cleitonbaloneker@gmail.com>
2023-01-21 19:50:18 -0300

    chore: add mock to localstorage

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,10 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     globals: true,
+    setupFiles: ['vitest-localstorage-mock'],
     mockReset: true,
     passWithNoTests: true,
+    environment: 'jsdom',
     exclude: ['**/node_modules/**', '**/dist/**', '**/*.test.ts', 'src'],
   },
   plugins: [tsconfigPaths()],
