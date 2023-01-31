fa449268f140be2a90cb6b12fe73457b18ed7d06
Cleiton Baloneker <cleiton.baloneker@a4pm.com.br>
2023-01-31 19:14:34 -0300

    update: Add getActivePomodoro dependency

--- src/presentation/modules/home/home-factory.tsx
+++ src/presentation/modules/home/home-factory.tsx
@@ -1,13 +1,17 @@
-import { setupStartPomodoroFocus } from '@/application/use-cases/start-pomodoro-focus';
+import { setupStartPomodoroFocus,setupGetActivePomodoro } from '@/application/use-cases';
 import { LocalStoragePomodoroRepository } from '@/infra/database/local-storage/repositories/local-storage-pomodoro-repository';
 import Home from './index';
+
+const localStoragePomodoroRepository = new LocalStoragePomodoroRepository();
+
+const startPomodoroFocus = setupStartPomodoroFocus(localStoragePomodoroRepository)
+const getActivePomodoro = setupGetActivePomodoro(localStoragePomodoroRepository)
 
 export const MakeHome: React.FC = () => {
   return (
     <Home
-      startPomodoroFocus={setupStartPomodoroFocus(
-        new LocalStoragePomodoroRepository()
-      )}
+      startPomodoroFocus={startPomodoroFocus}
+      getActivePomodoro={getActivePomodoro}
     />
   );
 };
