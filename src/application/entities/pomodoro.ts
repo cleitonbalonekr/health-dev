c55e2c5cee56e3294a22e148f0ddc488c1bf1119
cleitonbalonekr <cleitonbaloneker@gmail.com>
2023-02-02 22:04:09 -0300

    feat: Add methods to Pomodoro to verify stated and expired

--- src/application/entities/pomodoro.ts
+++ src/application/entities/pomodoro.ts
@@ -27,6 +27,17 @@ export class Pomodoro {
     return this.bookEndsAt();
   }
 
+  public wasStarted() {
+    return !!this.startsAt && !!this.endsAt;
+  }
+
+  public isExpired() {
+    if (!this.wasStarted()) {
+      throw new PomodoroException('Pomodoro was not started');
+    }
+    return (this.endsAt as Date) <= new Date() ? true : false;
+  }
+
   private bookEndsAt() {
     if (!this.startsAt) {
       throw new PomodoroException('the starts at is not defined');
--- tests/entities/pomodoro.spec.ts
+++ tests/entities/pomodoro.spec.ts
@@ -1,5 +1,6 @@
+import { PomodoroException } from '@/application/entities/errors/pomodoro-exception';
 import { makePomodoro } from '../application/factories/pomodoro-factory';
-import { addMinutes } from '../helpers';
+import { addMinutes, subMinutes } from '../helpers';
 
 describe('Pomodoro', () => {
   it('should create a new pomodoro with correct values', () => {
@@ -24,5 +25,43 @@ describe('Pomodoro', () => {
     expect(endsAt).toEqual(
       addMinutes(pomodoroStartsAt, pomodoro.timeToFocusInMinutes)
     );
+  });
+
+  describe('wasStarted', () => {
+    it('should return true when pomodoro was stated', () => {
+      const pomodoro = makePomodoro();
+
+      pomodoro.start();
+
+      expect(pomodoro.wasStarted()).toBeTruthy();
+    });
+    it('should return false when pomodoro was stated', () => {
+      const pomodoro = makePomodoro();
+
+      expect(pomodoro.wasStarted()).toBeFalsy();
+    });
+  });
+
+  describe('isEspired', () => {
+    it('should throw an PomodoroException when pomodoro was not started', () => {
+      const pomodoro = makePomodoro();
+
+      expect(pomodoro.isExpired.bind(pomodoro)).toThrow(
+        new PomodoroException('Pomodoro was not started')
+      );
+    });
+    it('should return true when pomodoro expired', () => {
+      const actualDate = new Date();
+      const pomodoro = makePomodoro({
+        startsAt: subMinutes(actualDate, 26),
+        endsAt: subMinutes(actualDate, 1),
+      });
+      expect(pomodoro.isExpired()).toBeTruthy();
+    });
+    it('should return false when pomodoro is not espired', () => {
+      const pomodoro = makePomodoro();
+      pomodoro.start();
+      expect(pomodoro.isExpired()).toBeFalsy();
+    });
   });
 });
