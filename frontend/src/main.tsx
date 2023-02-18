49fb9e2fb2f637d497cd1ba41b3183434a718866
cleitonbalonekr <cleitonbaloneker@gmail.com>
2023-02-18 15:36:55 -0300

    feat: Register firebase service worker to receive messages

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import 'firebase/config';
+import { firebaseConfig } from 'firebase/config';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+if ('serviceWorker' in navigator) {
+  const config = encodeURIComponent(JSON.stringify(firebaseConfig));
+  const swUrl = `../public/firebase-messaging-sw.js?${config}`;
+  navigator.serviceWorker
+    .register(swUrl)
+    .then(function (registration) {
+      console.log('Registration successful, scope is:', registration.scope);
+    })
+    .catch(function (err) {
+      console.log('Service worker registration failed, error:', err);
+    });
+}
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,30 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { VitePWA } from 'vite-plugin-pwa';
 
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [
+      react(),
+      tsconfigPaths(),
+      VitePWA({
+        injectRegister: 'auto',
+      }),
+    ],
+    define: {
+      FIREBASE_CONFIG: {
+        FIREBASE_API_KEY: env.VITE_FIREBASE_API_KEY,
+        FIREBASE_AUTH_DOMAIN: env.VITE_FIREBASE_AUTH_DOMAIN,
+        FIREBASE_PROJECT_ID: env.VITE_FIREBASE_PROJECT_ID,
+        FIREBASE_STORAGE_BUCKET: env.VITE_FIREBASE_STORAGE_BUCKET,
+        FIREBASE_MESSAGING_SENDER_ID: env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+        FIREBASE_APP_ID: env.VITE_FIREBASE_APP_ID,
+        FIREBASE_MEASUREMENT_ID: env.VITE_FIREBASE_MEASUREMENT_ID,
+      },
+    },
+  };
 });
