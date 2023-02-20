27f73651a347f355af7378bbb14569bec2c60057
cleitonbalonekr <cleitonbaloneker@gmail.com>
2023-02-20 10:57:44 -0300

    feat: Move registerToken to subscription page

--- frontend/src/presentation/modules/registration/index.tsx
+++ frontend/src/presentation/modules/registration/index.tsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { registerToken } from 'firebase';
 
 const Registration: React.FC = () => {
   const { extensionId } = useParams();
+  const [token, setToken] = useState('');
+
+  useEffect(() => {
+    registerToken(setToken);
+  }, []);
+
   return (
-    <div>
-      <h1>OLA</h1>
-      <p>{extensionId}</p>
+    <div className="flex flex-1 flex-col items-center justify-center h-screen">
+      {!token ? (
+        <h3>Carregando...</h3>
+      ) : (
+        <p className="text-justify">Tudo pronto</p>
+      )}
     </div>
   );
 };
