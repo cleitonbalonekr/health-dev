761abd439cbc5f96236f3a7f6e88bf0c8a8503da
cleitonbalonekr <cleitonbaloneker@gmail.com>
2023-02-27 20:36:56 -0300

    feat: Add Error page animation

--- frontend/src/presentation/modules/error/index.tsx
+++ frontend/src/presentation/modules/error/index.tsx
@@ -1,17 +1,27 @@
+import {
+  LOTTIE_BASE_OPTIONS,
+  LOTTIE_HEIGHT,
+  LOTTIE_WIDTH,
+} from '@/presentation/resources/base-settings';
 import React from 'react';
-import { useRouteError } from 'react-router-dom';
+import Lottie from 'react-lottie';
+import pageNotFoundAnimation from './animations/search-not-found.json';
 
 const ErrorPage: React.FC = () => {
-  const error = useRouteError() as any;
-  console.error(error);
-
   return (
-    <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
+    <div className="h-screen p-2 flex flex-1 flex-col items-center justify-center">
+      <h4 className="text-center text-black font-bold text-xl">
+        Ops! Parece que não tem nada por aqui.
+      </h4>
+
+      <Lottie
+        options={{
+          ...LOTTIE_BASE_OPTIONS,
+          animationData: pageNotFoundAnimation,
+        }}
+        height={LOTTIE_HEIGHT}
+        width={LOTTIE_WIDTH}
+      />
     </div>
   );
 };
---
+++ frontend/src/presentation/resources/base-settings.ts
@@ -0,0 +1,9 @@
+export const LOTTIE_BASE_OPTIONS = {
+  loop: true,
+  autoplay: true,
+  rendererSettings: {
+    preserveAspectRatio: 'xMidYMid slice',
+  },
+};
+export const LOTTIE_HEIGHT = 400;
+export const LOTTIE_WIDTH = 300;
