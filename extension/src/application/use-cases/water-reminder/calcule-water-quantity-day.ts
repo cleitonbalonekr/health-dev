dd2d6ee72129ff3e011dfdb4be0ba372baa2ab59
cleitonbalonekr <cleitonbaloneker@gmail.com>
2023-03-14 19:03:36 -0300

    feat: Ensure CalculeWaterQuantityDay calls WaterReminderRepository.save

--- extension/src/application/use-cases/water-reminder/calcule-water-quantity-day.ts
+++ extension/src/application/use-cases/water-reminder/calcule-water-quantity-day.ts
@@ -1,3 +1,5 @@
+import { WaterReminderRepository } from '@/application/repositories/water-reminder-repository';
+
 type Output = any;
 type Input = {
   weight: number;
@@ -5,13 +7,18 @@ type Input = {
 
 export type CalculeWaterQuantityDay = (input: Input) => Promise<Output>;
 
-type Setup = () => CalculeWaterQuantityDay;
+type Setup = (
+  waterReminderRepository: WaterReminderRepository
+) => CalculeWaterQuantityDay;
 
 export const setupCalculeWaterQuantityDay: Setup =
-  () =>
+  (waterReminderRepository) =>
   async ({ weight }) => {
     const waterInMlPerLiter = 35;
     const waterNecessaryInMl = weight * waterInMlPerLiter;
-    const waterNecessaryInLiters = (waterNecessaryInMl / 1000).toFixed(3);
+    const waterNecessaryInLiters = Number(
+      (waterNecessaryInMl / 1000).toFixed(3)
+    );
+    await waterReminderRepository.save(waterNecessaryInLiters);
     return waterNecessaryInLiters;
   };
--- extension/tests/application/use-cases/water-reminder/calcule-water-quantity-day.spec.ts
+++ extension/tests/application/use-cases/water-reminder/calcule-water-quantity-day.spec.ts
@@ -1,19 +1,31 @@
+import { WaterReminderRepository } from '@/application/repositories/water-reminder-repository';
 import {
   setupCalculeWaterQuantityDay,
   CalculeWaterQuantityDay,
 } from '@/application/use-cases/water-reminder';
+import { mock, MockProxy, mockReset } from 'vitest-mock-extended';
 describe('CalculeWaterQuantityDay', () => {
   let sut: CalculeWaterQuantityDay;
+  let waterReminderRepository: MockProxy<WaterReminderRepository>;
   const params = {
     weight: 61,
   };
-  beforeAll(() => {});
+  beforeAll(() => {
+    waterReminderRepository = mock();
+  });
   beforeEach(() => {
-    sut = setupCalculeWaterQuantityDay();
+    mockReset(waterReminderRepository);
+    sut = setupCalculeWaterQuantityDay(waterReminderRepository);
   });
 
   it('should return the quantity of water necessary by weight in liters', async () => {
+    const expectedValue = 2.135;
     const response = await sut(params);
-    expect(response).toBe(2.135);
+    expect(response).toBe(expectedValue);
+  });
+  it('should call WaterReminderRepository.save with correct values', async () => {
+    const response = await sut(params);
+    expect(waterReminderRepository.save).toHaveBeenCalledOnce();
+    expect(waterReminderRepository.save).toHaveBeenCalledWith(response);
   });
 });
