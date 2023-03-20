5426733273964600358e481edeb757cc5161079c
cleitonbalonekr <cleitonbaloneker@gmail.com>
2023-03-20 19:56:05 -0300

    refact: rename factories

--- extension/src/main/factories/use-cases/alarm/start-water-alarm-factory.ts
+++ extension/src/main/factories/use-cases/alarm/start-water-alarm-factory.ts
@@ -1,9 +1,9 @@
-import { setupStopAlarm } from '@/application/use-cases/alarm';
+import { setupStartWaterAlarm } from '@/application/use-cases/alarm';
 import { makeChromeAlarmService } from '@/main/factories/infra/gateways';
 import { makeChromeStorageAlarmRepository } from '@/main/factories/infra/repositories/chrome-storage';
 
-export const makeStopAlarm = () => {
-  return setupStopAlarm(
+export const makeStartWaterAlarm = () => {
+  return setupStartWaterAlarm(
     makeChromeAlarmService(),
     makeChromeStorageAlarmRepository()
   );
--- extension/src/main/factories/use-cases/alarm/stop-alarm-factory.ts
+++ extension/src/main/factories/use-cases/alarm/stop-alarm-factory.ts
@@ -1,9 +1,9 @@
-import { setupStartWaterAlarm } from '@/application/use-cases/alarm';
+import { setupStopAlarm } from '@/application/use-cases/alarm';
 import { makeChromeAlarmService } from '@/main/factories/infra/gateways';
 import { makeChromeStorageAlarmRepository } from '@/main/factories/infra/repositories/chrome-storage';
 
-export const makeStartWaterAlarm = () => {
-  return setupStartWaterAlarm(
+export const makeStopAlarm = () => {
+  return setupStopAlarm(
     makeChromeAlarmService(),
     makeChromeStorageAlarmRepository()
   );
