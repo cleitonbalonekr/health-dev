677bbe9d75c3d33f7ed4ca214427b8658df57bad
cleitonbalonekr <cleitonbaloneker@gmail.com>
2023-03-20 19:33:53 -0300

    update: Change session to local storage in ChromeStorageAlarmRepository

--- extension/src/infra/database/chrome-storage/repositories/chrome-storage-alarm-repository.ts
+++ extension/src/infra/database/chrome-storage/repositories/chrome-storage-alarm-repository.ts
@@ -4,17 +4,17 @@ import { ChromeStorageAlarmMapper } from '../mappers/chrome-storage-alarm-mapper
 
 export class ChromeStorageAlarmRepository implements AlarmRepository {
   async getByType(type: AlarmType): Promise<Alarm | null> {
-    const raw = await chrome.storage.session.get(type);
+    const raw = await chrome.storage.local.get(type);
     if (!raw[type]) return null;
     return ChromeStorageAlarmMapper.toAlarm(raw[type]);
   }
 
   async save(alarm: Alarm): Promise<void> {
     const raw = ChromeStorageAlarmMapper.toChromeStorage(alarm);
-    await chrome.storage.session.set({ [alarm.type]: raw });
+    await chrome.storage.local.set({ [alarm.type]: raw });
   }
 
   async remove(type: AlarmType): Promise<void> {
-    await chrome.storage.session.remove(type);
+    await chrome.storage.local.remove(type);
   }
 }
